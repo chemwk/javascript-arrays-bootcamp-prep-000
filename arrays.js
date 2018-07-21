b2fe803aa1e363e38f87b98a93a470db3f2a5d74
min kim <minwookkim2013@u.northwestern.edu>
2018-07-21 07:04:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,6 +14,5 @@ function addElementToBeginningOfArray (array,element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element){
-  array.unshift(element)
-  return array
+  return array.unshift(element)
 }

a5926703dca78da4d1fd97ded0c3e635d823a51d
min kim <minwookkim2013@u.northwestern.edu>
2018-07-21 06:38:18 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,20 @@
+var ingredient1 = "bread"
+var ingredient2 = "mild cheese"
+var ingredient3 = "sharp cheese"
+var ingredient4 = "butter"
+var ingredient5 = "tomato"
+var ingredient6 = "garlic"
+var grilledCheeseIngredients = [ 'bread', 'mild cheese', 'sharp cheese', 'butter', 'tomato', 'garlic']
+var tomatoSauceIngredients = [ 'tomato', 'garlic', 'olive oil', 'basil', 'oregano']
+var chocolateBars = ['snickers','hundred rand', 'kitkat', 'skittles']
+
+
+function addElementToBeginningOfArray (array,element) {
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element){
+  array = [element, ...array]
+  array[0] = element
+  return array
+}

42ff98db8d0a894c600e850d890cd411f4402bc0
min kim <minwookkim2013@u.northwestern.edu>
2018-07-21 07:45:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -44,6 +44,12 @@ function removeElementFromBeginningOfArray(array){
   return array.slice(1)
 }
 
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+
 function accessElementInArray(){
   
 }
